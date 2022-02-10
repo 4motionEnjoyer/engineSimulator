96e2ac5eec8a16ae27af46bf2b8eede955552cd2
4motionEnjoyer <99203040+4motionEnjoyer@users.noreply.github.com>
2022-02-10 23:12:08 +0200

    Add files via upload

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -36,116 +36,121 @@ namespace moottoriSimulaattori
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.radioButton_cylinder4 = new System.Windows.Forms.RadioButton();
-			this.radioButton_cylinder3 = new System.Windows.Forms.RadioButton();
-			this.radioButton_cylinder2 = new System.Windows.Forms.RadioButton();
-			this.radioButton_cylinder1 = new System.Windows.Forms.RadioButton();
-			this.progressBar_RPM = new System.Windows.Forms.ProgressBar();
-			this.label_RPM = new System.Windows.Forms.Label();
-			this.label1 = new System.Windows.Forms.Label();
-			this.button_accPedal = new System.Windows.Forms.Button();
-			this.button_ignition = new System.Windows.Forms.Button();
-			this.SuspendLayout();
-			// 
-			// radioButton_cylinder4
-			// 
-			this.radioButton_cylinder4.Location = new System.Drawing.Point(429, 33);
-			this.radioButton_cylinder4.Name = "radioButton_cylinder4";
-			this.radioButton_cylinder4.Size = new System.Drawing.Size(104, 24);
-			this.radioButton_cylinder4.TabIndex = 0;
-			this.radioButton_cylinder4.TabStop = true;
-			this.radioButton_cylinder4.Text = "Cylinder 4";
-			this.radioButton_cylinder4.UseVisualStyleBackColor = true;
-			// 
-			// radioButton_cylinder3
-			// 
-			this.radioButton_cylinder3.Location = new System.Drawing.Point(429, 63);
-			this.radioButton_cylinder3.Name = "radioButton_cylinder3";
-			this.radioButton_cylinder3.Size = new System.Drawing.Size(104, 24);
-			this.radioButton_cylinder3.TabIndex = 1;
-			this.radioButton_cylinder3.TabStop = true;
-			this.radioButton_cylinder3.Text = "Cylinder 3";
-			this.radioButton_cylinder3.UseVisualStyleBackColor = true;
-			// 
-			// radioButton_cylinder2
-			// 
-			this.radioButton_cylinder2.Location = new System.Drawing.Point(429, 94);
-			this.radioButton_cylinder2.Name = "radioButton_cylinder2";
-			this.radioButton_cylinder2.Size = new System.Drawing.Size(104, 24);
-			this.radioButton_cylinder2.TabIndex = 2;
-			this.radioButton_cylinder2.TabStop = true;
-			this.radioButton_cylinder2.Text = "Cylinder 2";
-			this.radioButton_cylinder2.UseVisualStyleBackColor = true;
-			// 
-			// radioButton_cylinder1
-			// 
-			this.radioButton_cylinder1.Location = new System.Drawing.Point(429, 125);
-			this.radioButton_cylinder1.Name = "radioButton_cylinder1";
-			this.radioButton_cylinder1.Size = new System.Drawing.Size(104, 24);
-			this.radioButton_cylinder1.TabIndex = 3;
-			this.radioButton_cylinder1.TabStop = true;
-			this.radioButton_cylinder1.Text = "Cylinder 1";
-			this.radioButton_cylinder1.UseVisualStyleBackColor = true;
-			// 
-			// progressBar_RPM
-			// 
-			this.progressBar_RPM.Location = new System.Drawing.Point(31, 34);
-			this.progressBar_RPM.Name = "progressBar_RPM";
-			this.progressBar_RPM.Size = new System.Drawing.Size(287, 23);
-			this.progressBar_RPM.TabIndex = 4;
-			// 
-			// label_RPM
-			// 
-			this.label_RPM.Location = new System.Drawing.Point(31, 5);
-			this.label_RPM.Name = "label_RPM";
-			this.label_RPM.Size = new System.Drawing.Size(100, 23);
-			this.label_RPM.TabIndex = 5;
-			this.label_RPM.Text = "RPM";
-			// 
-			// label1
-			// 
-			this.label1.Location = new System.Drawing.Point(31, 64);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(287, 23);
-			this.label1.TabIndex = 6;
-			this.label1.Text = "0 . . . 1k . . . 2k . . . 3k . . . 4k . . . 5k . . . 6k";
-			// 
-			// button_accPedal
-			// 
-			this.button_accPedal.Location = new System.Drawing.Point(31, 138);
-			this.button_accPedal.Name = "button_accPedal";
-			this.button_accPedal.Size = new System.Drawing.Size(100, 41);
-			this.button_accPedal.TabIndex = 7;
-			this.button_accPedal.Text = "Acc. pedal";
-			this.button_accPedal.UseVisualStyleBackColor = true;
-			// 
-			// button_ignition
-			// 
-			this.button_ignition.Location = new System.Drawing.Point(171, 138);
-			this.button_ignition.Name = "button_ignition";
-			this.button_ignition.Size = new System.Drawing.Size(101, 41);
-			this.button_ignition.TabIndex = 8;
-			this.button_ignition.Text = "Ignition";
-			this.button_ignition.UseVisualStyleBackColor = true;
-			this.button_ignition.Click += new System.EventHandler(this.Button_ignitionClick);
-			// 
-			// MainForm
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(1067, 504);
-			this.Controls.Add(this.button_ignition);
-			this.Controls.Add(this.button_accPedal);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.label_RPM);
-			this.Controls.Add(this.progressBar_RPM);
-			this.Controls.Add(this.radioButton_cylinder1);
-			this.Controls.Add(this.radioButton_cylinder2);
-			this.Controls.Add(this.radioButton_cylinder3);
-			this.Controls.Add(this.radioButton_cylinder4);
-			this.Name = "MainForm";
-			this.Text = "moottoriSimulaattori";
-			this.ResumeLayout(false);
+            this.radioButton_cylinder4 = new System.Windows.Forms.RadioButton();
+            this.radioButton_cylinder3 = new System.Windows.Forms.RadioButton();
+            this.radioButton_cylinder2 = new System.Windows.Forms.RadioButton();
+            this.radioButton_cylinder1 = new System.Windows.Forms.RadioButton();
+            this.progressBar_RPM = new System.Windows.Forms.ProgressBar();
+            this.label_RPM = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_accPedal = new System.Windows.Forms.Button();
+            this.button_ignition = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // radioButton_cylinder4
+            // 
+            this.radioButton_cylinder4.Location = new System.Drawing.Point(429, 33);
+            this.radioButton_cylinder4.Name = "radioButton_cylinder4";
+            this.radioButton_cylinder4.Size = new System.Drawing.Size(104, 24);
+            this.radioButton_cylinder4.TabIndex = 0;
+            this.radioButton_cylinder4.TabStop = true;
+            this.radioButton_cylinder4.Text = "Cylinder 4";
+            this.radioButton_cylinder4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_cylinder3
+            // 
+            this.radioButton_cylinder3.Location = new System.Drawing.Point(429, 63);
+            this.radioButton_cylinder3.Name = "radioButton_cylinder3";
+            this.radioButton_cylinder3.Size = new System.Drawing.Size(104, 24);
+            this.radioButton_cylinder3.TabIndex = 1;
+            this.radioButton_cylinder3.TabStop = true;
+            this.radioButton_cylinder3.Text = "Cylinder 3";
+            this.radioButton_cylinder3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_cylinder2
+            // 
+            this.radioButton_cylinder2.Location = new System.Drawing.Point(429, 94);
+            this.radioButton_cylinder2.Name = "radioButton_cylinder2";
+            this.radioButton_cylinder2.Size = new System.Drawing.Size(104, 24);
+            this.radioButton_cylinder2.TabIndex = 2;
+            this.radioButton_cylinder2.TabStop = true;
+            this.radioButton_cylinder2.Text = "Cylinder 2";
+            this.radioButton_cylinder2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_cylinder1
+            // 
+            this.radioButton_cylinder1.Location = new System.Drawing.Point(429, 125);
+            this.radioButton_cylinder1.Name = "radioButton_cylinder1";
+            this.radioButton_cylinder1.Size = new System.Drawing.Size(104, 24);
+            this.radioButton_cylinder1.TabIndex = 3;
+            this.radioButton_cylinder1.TabStop = true;
+            this.radioButton_cylinder1.Text = "Cylinder 1";
+            this.radioButton_cylinder1.UseVisualStyleBackColor = true;
+            // 
+            // progressBar_RPM
+            // 
+            this.progressBar_RPM.Location = new System.Drawing.Point(31, 34);
+            this.progressBar_RPM.Name = "progressBar_RPM";
+            this.progressBar_RPM.Size = new System.Drawing.Size(287, 23);
+            this.progressBar_RPM.TabIndex = 4;
+            // 
+            // label_RPM
+            // 
+            this.label_RPM.Location = new System.Drawing.Point(31, 5);
+            this.label_RPM.Name = "label_RPM";
+            this.label_RPM.Size = new System.Drawing.Size(100, 23);
+            this.label_RPM.TabIndex = 5;
+            this.label_RPM.Text = "RPM";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(31, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(287, 23);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "0 . . . 1k . . . 2k . . . 3k . . . 4k . . . 5k . . . 6k";
+            // 
+            // button_accPedal
+            // 
+            this.button_accPedal.Location = new System.Drawing.Point(31, 138);
+            this.button_accPedal.Name = "button_accPedal";
+            this.button_accPedal.Size = new System.Drawing.Size(100, 41);
+            this.button_accPedal.TabIndex = 7;
+            this.button_accPedal.Text = "Acc. pedal";
+            this.button_accPedal.UseVisualStyleBackColor = true;
+            this.button_accPedal.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button_accPedal_MouseDown);
+            this.button_accPedal.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button_accPedal_MouseUp);
+            // 
+            // button_ignition
+            // 
+            this.button_ignition.Location = new System.Drawing.Point(171, 138);
+            this.button_ignition.Name = "button_ignition";
+            this.button_ignition.Size = new System.Drawing.Size(101, 41);
+            this.button_ignition.TabIndex = 8;
+            this.button_ignition.Text = "Ignition";
+            this.button_ignition.UseVisualStyleBackColor = true;
+            this.button_ignition.Click += new System.EventHandler(this.Button_ignitionClick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1062, 503);
+            this.Controls.Add(this.button_ignition);
+            this.Controls.Add(this.button_accPedal);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label_RPM);
+            this.Controls.Add(this.progressBar_RPM);
+            this.Controls.Add(this.radioButton_cylinder1);
+            this.Controls.Add(this.radioButton_cylinder2);
+            this.Controls.Add(this.radioButton_cylinder3);
+            this.Controls.Add(this.radioButton_cylinder4);
+            this.MinimumSize = new System.Drawing.Size(1080, 550);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "moottoriSimulaattori";
+            this.ResumeLayout(false);
+
 		}
 		private System.Windows.Forms.Button button_ignition;
 		private System.Windows.Forms.Button button_accPedal;
@@ -156,5 +161,5 @@ namespace moottoriSimulaattori
 		private System.Windows.Forms.RadioButton radioButton_cylinder2;
 		private System.Windows.Forms.RadioButton radioButton_cylinder3;
 		private System.Windows.Forms.RadioButton radioButton_cylinder4;
-	}
+    }
 }
--- MainForm.cs
+++ MainForm.cs
@@ -3,6 +3,21 @@
 /// 
 /// Changelog:
 /// V0.001 New project created. Buttons and tacho created alongside rButtons for cylinder bangs. Initial logic for cr bgworker.
+/// V0.002 Crankshaft rotation simulator done (backgroundworker), physics engine (sound at this time) plays sound in accordance to rpm.
+/// V0.003 Sounds work for idle and acceleration, but not for decel. Radiobuttons in accordance to cylinder firing done. Accel button raises rpm like in real life.
+/// V0.004 Added pop-bang decel.
+/// 
+/// To be added:
+/// Audio file pathfinding to work better perhaps
+/// Possibly decel sounds
+/// Bugfixes and testing
+/// 
+/// To be added in far future:
+/// Realistic physic modeling for the effect of injection quantity and air mass / stroke to torque output
+/// Actual torque and power calculation
+/// Realistic turbo with variable nozzle turbine
+/// 
+///
 /// </summary>
 
 using System;
@@ -13,6 +28,8 @@ using System.Drawing;
 using System.Windows.Forms;
 using System.Threading;
 using System.Diagnostics;
+using System.Timers;
+using System.Windows.Media;
 
 
 namespace moottoriSimulaattori
@@ -20,8 +37,21 @@ namespace moottoriSimulaattori
 
 	public partial class MainForm : Form
 	{
-		BackgroundWorker bg_crankshaft;
+		public struct motorState
+        {
+			public double crAngle;
+			public double firingCylinder;
+        }
+
+		BackgroundWorker bg_crankshaft;			//simulation of crankshaft revolutions
+		BackgroundWorker bg_physicsEngine;          //simulation of events happening in relation to cr rotation
+		BackgroundWorker bg_increaseRPM;			//because of C# limitations, we need a thread to increase rpm like in real life (held down -> rpm++, release -> rpm--)
+		double specRPS = 13.3;              //800 rpm / 60 = 13,3 cr revolutions per second (hz), max rpm 5500 = 91,667 rps cr (hz)
+		motorState engine = new motorState();
+		bool accPedalPress = false;
 		
+
+
 		public MainForm()
 		{
 			InitializeComponent();
@@ -32,9 +62,25 @@ namespace moottoriSimulaattori
             bg_crankshaft.ProgressChanged += new ProgressChangedEventHandler(bg_crankshaft_ProgressChanged);
             bg_crankshaft.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bg_crankshaft_RunWorkerCompleted);
             bg_crankshaft.WorkerReportsProgress = true;
-            bg_crankshaft.WorkerSupportsCancellation = true;		//event handlers and properties
-			
-			
+            bg_crankshaft.WorkerSupportsCancellation = true;        //event handlers and properties
+
+			bg_physicsEngine = new BackgroundWorker();  //creating a bg worker (thread) for our cr rotation
+
+			bg_physicsEngine.DoWork += new DoWorkEventHandler(bg_physicsEngine_DoWork);
+			bg_physicsEngine.ProgressChanged += new ProgressChangedEventHandler(bg_physicsEngine_ProgressChanged);
+			bg_physicsEngine.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bg_physicsEngine_RunWorkerCompleted);
+			bg_physicsEngine.WorkerReportsProgress = true;
+			bg_physicsEngine.WorkerSupportsCancellation = true;        //event handlers and properties
+
+			bg_increaseRPM = new BackgroundWorker();
+
+			bg_increaseRPM.DoWork += new DoWorkEventHandler(bg_increaseRPM_DoWork);
+			bg_increaseRPM.ProgressChanged += new ProgressChangedEventHandler(bg_increaseRPM_ProgressChanged);
+			bg_increaseRPM.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bg_increaseRPM_RunWorkerCompleted);
+			bg_increaseRPM.WorkerReportsProgress = true;
+			bg_increaseRPM.WorkerSupportsCancellation = true;        //event handlers and properties
+
+
 		}
 		
 		void bg_crankshaft_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
@@ -42,61 +88,217 @@ namespace moottoriSimulaattori
             if (e.Cancelled)		//if thread exits and it has been cancelled
             {
                 MessageBox.Show("Engine was turned off");
-            }
+			}
 
             if (e.Error != null)
             {
             	MessageBox.Show("It seems that the engine died for some reason... " + e.Error.Data);
             }
-
+			progressBar_RPM.Value = 0;
         }
 		
 		void bg_crankshaft_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
-            progressBar_RPM.Value = e.ProgressPercentage;  //bg worker cant paint ui thread so this does it. Sounds here? 
-            
+
+			progressBar_RPM.Value = 13;
         }
-		
+
 		void bg_crankshaft_DoWork(object sender, DoWorkEventArgs e)
         {
-			//logic here
-			double crankshaftPosition;
-			double t = 1;
-			double frequency = 1;
-			Stopwatch runTime = new Stopwatch();
-			runTime.Start();
-			
-			double[] log = new Double[99999];
-			int i_log = 0;
-			
-			while(!bg_crankshaft.CancellationPending)
-			{
-				//t = runTime.ElapsedMilliseconds;
-				t++;
-				crankshaftPosition = (1 * Math.Sin((2 * Math.PI * frequency * t)));
-				
-				log[i_log] = crankshaftPosition;
-				i_log++;
-				Thread.Sleep(1);
-				if(i_log > 100)
+			Stopwatch runningTime = new Stopwatch();				//for sine function (rotation) we need a running time
+			double elapsedSeconds = 0;
+			double crankshaftAngle = 0;
+			double ms = 0;
+
+			/*
+			double[] log = new double[99999];
+			int i = 0;
+			double[] slog = new double[99999];
+
+			 */
+
+			runningTime.Start();
+
+			bg_crankshaft.ReportProgress(0);
+			engine.firingCylinder = 1;
+
+			while (!bg_crankshaft.CancellationPending)
+            {
+				ms = runningTime.Elapsed.TotalMilliseconds;		//accuracy
+				elapsedSeconds = ms / 1000;						//conversion to seconds
+				crankshaftAngle = (1 * (Math.Asin(Math.Sin(2 * 3.14 * specRPS * elapsedSeconds)))) * (180 / 3.14);    //crankshaft spinning simulation, it can be modeled as a phasor with a constant amp
+				engine.crAngle = crankshaftAngle;                                                  //this simulates crankshaft pos. sensor to ecu
+
+                #region logging/testing
+                /*
+				slog[i] = elapsedSeconds;
+				log[i] = crankshaftAngle;
+				i++;
+				if (i == 99999)
 					break;
+				*/
+                #endregion
+            }
+			e.Cancel = true;			//because of a race condition, this flag has to be set manually. I am not certainly sure why...
+        }
+
+		void bg_physicsEngine_DoWork(object sender, DoWorkEventArgs e)
+        {
+			bool soundLatch = false;            //latching for sound so no more than 1 is plaid when hitting TDC of cyl. 
+			bool bangLatch = false;
+			double old_specRPS = 0;
+			Stopwatch bangTimer = new Stopwatch();
+		
+			MediaPlayer cyl1 = new MediaPlayer();
+			MediaPlayer cyl2 = new MediaPlayer();
+			MediaPlayer popbang1 = new MediaPlayer();
+			MediaPlayer popbang2 = new MediaPlayer();
+			cyl1.Open(new System.Uri(@"C:\Users\leevi\Documents\SharpDevelop Projects\moottoriSimulaattori\moottoriSimulaattori\Sound\tdishort.wav"));
+			cyl2.Open(new System.Uri(@"C:\Users\leevi\Documents\SharpDevelop Projects\moottoriSimulaattori\moottoriSimulaattori\Sound\tdishort.wav"));
+			popbang1.Open(new System.Uri(@"C:\Users\leevi\Documents\SharpDevelop Projects\moottoriSimulaattori\moottoriSimulaattori\Sound\popbang1.wav"));
+			popbang2.Open(new System.Uri(@"C:\Users\leevi\Documents\SharpDevelop Projects\moottoriSimulaattori\moottoriSimulaattori\Sound\popbang2.wav"));
+
+
+			while (!bg_physicsEngine.CancellationPending)
+            {
+				old_specRPS = specRPS;					//with this helper var we can analyze if the rpm is increasing or decreasing
+
+				if(engine.crAngle > 87 && soundLatch == false)			//4 cylinders mean that there are two reciprocating pairs. One power stroke happens every 180* of crankshaft rotation
+                {                                                       //however, this approximation is a bit wrong as 4cyl engines the piston doesn't move with linear speed, it is faster near BDC, but this approx is good enough for now
+					cyl1.Play();										//the positive cr TDC is to cyls 1 and 4
+					Thread.Sleep(1000 / (int)specRPS);					//I suppose this could be done by events (1 piston firing --> event cylinder fired)
+					cyl1.Stop();
+					if (engine.firingCylinder == 1)				//check which cylinder has been fired and which is next. 1.9 TDI uses 1-3-4-2 firing order
+						engine.firingCylinder = 3;
+					if (engine.firingCylinder == 4)
+						engine.firingCylinder = 2;
+				}
+
+				if(engine.crAngle < -87 && soundLatch == false)			//this is cylinders 2 and 3
+                {
+					cyl2.Play();
+					Thread.Sleep(1000 / (int)specRPS);
+					cyl2.Stop();
+					if (engine.firingCylinder == 3)
+						engine.firingCylinder = 4;
+					if (engine.firingCylinder == 2)
+						engine.firingCylinder = 1;
+				}
+
+				if (engine.crAngle < 87 || engine.crAngle > -87)
+					soundLatch = false;
+
+				if(accPedalPress == false && specRPS > 14.3)			//when accelerator is depressed, the revs will lower as the torque requested from engine will be enough to idle it.
+                {
+					specRPS = specRPS - 2;
+					Thread.Sleep(20);
+                }
+
+				if(specRPS + 1 < old_specRPS && old_specRPS > 40 && bangLatch == false)			//if the rpm is falling and it has been high enough
+                {
+					bangTimer.Start();
+					popbang1.Play();
+					bangLatch = true;														//we get bangs from exhaust
+                }
+
+				if (specRPS < 40)
+				{
+					bangLatch = false;
+					popbang1.Stop();
+				}
+
+				if (bangTimer.ElapsedMilliseconds > 2000)
+				{
+					popbang1.Stop();
+					bangTimer.Reset();
+				}
+					bg_physicsEngine.ReportProgress(0);			//used for updating the tachometer
+
 			}
-			
-			
-			
 		}
+	
+		void bg_physicsEngine_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+			//13,3 . . . 91 rps range, oddly the progress bar percentage is similar to what wanted. Not exactly tho and too lazy to make a look-up-table
+			progressBar_RPM.Value = (int)specRPS;
+
+			if (engine.firingCylinder == 1)
+				radioButton_cylinder1.Checked = true;
+			if (engine.firingCylinder == 2)
+				radioButton_cylinder2.Checked = true;
+			if (engine.firingCylinder == 3)
+				radioButton_cylinder3.Checked = true;
+			if (engine.firingCylinder == 4)
+				radioButton_cylinder4.Checked = true;
+
+
+
+		}
+
+		void bg_physicsEngine_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+
+        }
+
+		void bg_increaseRPM_DoWork(object sender, DoWorkEventArgs e)
+        {
+			while(accPedalPress == true)
+            {
+				if (specRPS > 91)
+					specRPS = 91;
+				if(specRPS < 91)		//RPMS rise slower under boost threshold, might add this later, for time being the increase is linear
+					specRPS = specRPS + 2;
+				//if(specRPS < 91 && specRPS > 34)		//When boost threshold (now set around 2k rpm, the RPM will rise faster as the turbo make it possible to inject more fuel without the stoichiometric ratio falling
+                //{						//In real life the rise is very different but in this showing purpose, the acceleration is merely doubled
+				//	specRPS = specRPS + 2;
+                //}
+				Thread.Sleep(20);
+            }
+        }
+
+		void bg_increaseRPM_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+
+        }
+
+		void bg_increaseRPM_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+
+        }
 		        
 		void Button_ignitionClick(object sender, EventArgs e)
 		{
-			 /*System.Media.SoundPlayer player = 
-			 new System.Media.SoundPlayer();
-			 player.SoundLocation = @"C:\Users\Public\Music\Sample Music\xxxx.wav";
-			 player.Load();
-			 player.Play();*/
-			 bg_crankshaft.RunWorkerAsync();
+			if (!bg_crankshaft.IsBusy)
+				bg_crankshaft.RunWorkerAsync();
+			else
+				bg_crankshaft.CancelAsync();
+
+			if (!bg_physicsEngine.IsBusy)
+				bg_physicsEngine.RunWorkerAsync();
+			else
+				bg_physicsEngine.CancelAsync();
 			
 		}
-		
 
-	}
+        private void button_accPedal_MouseUp(object sender, MouseEventArgs e)
+        {
+			accPedalPress = false;
+        }
+
+        private void button_accPedal_MouseDown(object sender, MouseEventArgs e)
+        {
+			accPedalPress = true;
+			bg_increaseRPM.RunWorkerAsync();
+		}
+    }
 }
+
+
+/*	while(!bg_crankshaft.CancellationPending)
+{
+System.Timers.Timer ignitionEvent = new System.Timers.Timer((1000 / specRPM));
+ignitionEvent.Elapsed += new ElapsedEventHandler(ignitionEventHandler);
+ignitionEvent.Enabled = true;
+
+}
+*/
